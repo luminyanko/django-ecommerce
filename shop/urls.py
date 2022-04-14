da60c250dda124c94a4444ca43b23ed606c4f0d8
iHnKwuuN <6nMnueBijq>
2022-04-14 22:20:21 +0900

    users added

--- shop/urls.py
+++ shop/urls.py
@@ -4,6 +4,14 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('<slug:category_slug>', views.home, name='products_by_category'),
-    path('product/', views.product, name='product')
+    path('category/<slug:category_slug>', views.home, name='products_by_category'),
+    path('category/<slug:category_slug>/<slug:product_slug>', views.product, name='product_detail' ),
+    path('product/', views.product, name='product'),
+    path('cart/', views.cart_detail, name='cart_detail'),
+    path('cart/add/<int:product_id>', views.add_cart, name='add_cart'),
+    path('cart/remove/<int:product_id>', views.cart_remove, name='cart_remove'),
+    path('cart/delete/<int:product_id>', views.cart_delete, name='cart_delete'),
+    path('account/create/', views.signUpView, name='signup'),
+    path('account/login/', views.loginView, name='login'),
+    path('account/logout/', views.logoutView, name='logout'),
 ]
